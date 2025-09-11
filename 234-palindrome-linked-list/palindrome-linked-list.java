72b04d18b966c138d54a8f1f3c2f6e2425902408
Sneha Shukla <snehashukla0815@gmail.com>
2025-09-11 11:51:52 +0530

    Time: 4 ms (83.50%) | Memory: 69.2 MB (11.02%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,29 +9,38 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head){
+       ListNode after= null;
+       ListNode curr = head;
+       ListNode prev = null;
+       while(curr!=null){
+        after = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = after;
+
+     }
+       return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        if (head == null) return true;
-
-        Deque<Integer> stack = new ArrayDeque<>();
-        ListNode current = head;
-
-        // First pass: push all values onto the stack
-        while (current != null) {
-            stack.push(current.val);
-            current = current.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-
-        // Second pass: compare node values against stack
-        current = head;
-        while (current != null) {
-            if (current.val != stack.pop()) {
-                return false;
-            }
-            current = current.next;
+        ListNode temp = reverse(slow.next);
+        slow.next = temp;
+        ListNode p1 = head;
+        ListNode p2 = slow.next;
+        while(p2!=null){
+            if(p1.val!= p2.val)return false;
+            p1 = p1.next;
+            p2 = p2.next;
         }
-
         return true;
 
+        
         
     }
 }
