e497ab53c60723181c2c3b848b15c6fdcf3d3f17
Sneha Shukla <snehashukla0815@gmail.com>
2025-12-27 14:04:32 +0530

    Time: 4 ms (99.93%) | Memory: 45.7 MB (85.41%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+    if(x<0){
+        return false;
+
+    }
+    int n=x;
+    int reverse=0;
+    while(n!=0){
+        int digit=n%10;
+        reverse=reverse*10+digit;
+         n=n/10;
+    }
+    if(reverse==x){
+        return true;
+    }else{
+        return false;
+    }
+
+
+    }
+}
