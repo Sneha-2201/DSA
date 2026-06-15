57a55461baca52aaf69a567abd2b6d9cb60da42b
Sneha Shukla <snehashukla0815@gmail.com>
2026-06-15 21:11:11 +0530

    Time: 4 ms (19.31%) | Memory: 202.4 MB (87.76%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -3,8 +3,8 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
+ *     ListNod(int val) { this.val = val; }
+ *     ListNodee() {}
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
@@ -15,13 +15,14 @@ class Solution {
         }
         ListNode slow = head;
         ListNode fast = head;
-        while( fast.next.next!= null && fast.next.next.next!=null){
+        while(fast.next.next!=null && fast.next.next.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            
+
         }
-        slow.next = slow.next.next;
+
         
+            slow.next = slow.next.next;
         return head;
         
     }
